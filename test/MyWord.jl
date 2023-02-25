04709a5493b7e835563d70c391ce813fd1524a4c
Jan Gontscharow <jan.gontscharow@gmail.com>
2023-02-25 14:13:49 +0100

    test for macros added

--- test/MyWord.jl
+++ test/MyWord.jl
@@ -1,33 +1,34 @@
-@testset "Words" begin
-    #w = word"a^-1ab"
-    #v = word"b"
+@testset "Word macro" begin
+    @test P.word"ab" == P.MyWord([1,2])
+    @test P.word"a^-1" == P.MyWord([-1])
+    @test P.word"" == P.MyWord()
+    @test P.word"a^-2c^1b^0" == P.MyWord([-1,-1,3])
+end
 
-    #w = MyPackage.word"a^-1ab"
-    w = MyPackage.MyWord([-1, 1, 2])
-    v = MyPackage.MyWord([2])
+@testset "Words" begin
+    w = P.word"a^-1ab"
+    v = P.word"b"
+
     @test isone(one(w))
     @test length(w) == 3
     @test length(v) == 1
     
-    #@test inv(w) == word"b^-1a^-1a"
-    @test inv(w) == MyPackage.MyWord([-2, -1, 1])
-    #@test w*v == word"a^-1abb" 
-    @test w*v == MyPackage.MyWord([-1, 1, 2, 2])
-    @test MyPackage.degree(w) == 2
-    @test MyPackage.degree(v) == 2
+    @test inv(w) == P.word"b^-1a^-1a"
+    @test w*v == P.word"a^-1abb" 
+    @test P.degree(w) == 2
+    @test P.degree(v) == 2
     
-    @test MyPackage.run_decomposition(w*v) == [(-1, 1), (1, 1), (2, 2)]
+    @test P.run_decomposition(w*v) == [(-1, 1), (1, 1), (2, 2)]
     #@test show(w*v) == "a^-1ab^2" 
 
-    s1 = MyPackage.MyWord([1])
-    s2 = MyPackage.MyWord([2])
-    u = s1*s2*s1*s2*s1*s2
-    @test MyPackage.isprefix(s1*s2, u)
-    @test !MyPackage.isprefix(s2*s1, u)
-    @test MyPackage.issuffix(s2*s1*s2, u)
-    @test !MyPackage.issuffix(s1, u)
-    @test length(MyPackage.suffixes(u)) == length(MyPackage.prefixes(u)) == 6
-    @test length(MyPackage.proper_subwords(u)) == 10
+    a = P.word"a"
+    b = P.word"b"
+    u = a*b*a*b*a*b
+    @test P.isprefix(a*b, u)
+    @test !P.isprefix(b*a, u)
+    @test P.issuffix(b*a*b, u)
+    @test !P.issuffix(a, u)
+    @test length(P.suffixes(u)) == length(P.prefixes(u)) == 6
+    @test length(P.proper_subwords(u)) == 10
     @test w == copy(w)
-
 end
--- test/Presentation.jl
+++ test/Presentation.jl
@@ -1,23 +1,30 @@
-@testset "Presentation" begin
-    r1 = MyPackage.MyWord([-1, -1, 2, 3])
-    r2 = MyPackage.MyWord([-2, 3, 1])
-    Π = MyPackage.Presentation([r1, r2])
-
-    @test MyPackage.gens(Π) == 1:3
-    @test MyPackage.rel(Π) == [r2,r1]
+@testset "Presentation macro" begin
+    Π = P.pres"|a^2, bb,c^2"
+    @test P.rel(Π) == [P.word"aa", P.word"bb", P.word"cc"]
     
-    w1 = MyPackage.MyWord([-2, -2, 1, 3])
-    w2 = MyPackage.MyWord([-1, 3, 2])
-    Π2 = MyPackage.Presentation([w1,w2])
-    MyPackage.relabel!(Π, 1, 2)
-    @test MyPackage.rel(Π) == MyPackage.rel(Π2)
+    Π = P.pres"a, b |"
+    @test isempty(P.rel(Π))
+    @test P.deg(Π) == 2
+    
+    Π = P.pres"d| a^2"
+    @test P.rel(Π) == [P.word"aa"]
+    @test P.deg(Π) == 4
+end
 
-    #v1 = MyPackage.MyWord([-3, -3, 2, 1])
-    #v2 = MyPackage.MyWord([-2, 1, 3])
-    #Π2 = MyPackage.Presentation([v1,v2])
-    #MyPackage.relabel!(Π, Dict(1=>2, 2=>3, 3=>1))
-    #@test MyPackage.rel(Π) == MyPackage.rel(Π2)
+@testset "Presentation" begin
+    r1 = P.word"a^-2bc"
+    r2 = P.word"b^-1ca"
+    Π = P.Presentation([r1, r2])
 
-    w = MyPackage.MyWord([1, 2, -2, 3, 3, -3, 4, 1, -1, -3, -1])
-    @test MyPackage.cyclic_rewrite!(one(w), w) == MyPackage.MyWord([4])
+    @test P.gens(Π) == 1:3
+    @test P.rel(Π) == [r2,r1]
+    
+    w1 = P.word"b^-2ac"
+    w2 = P.word"a^-1cb"
+    Π2 = P.Presentation([w1,w2])
+    P.relabel!(Π, 1, 2)
+    @test P.rel(Π) == P.rel(Π2)
+
+    w = P.word"abb^-1ccc^-1daa^-1c^-1a^-1"
+    @test P.cyclic_rewrite!(one(w), w) == P.word"d"
 end
