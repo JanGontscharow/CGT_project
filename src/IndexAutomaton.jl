66ccf30e872c5a4180db2f487467193f5bd17b26
Jan Gontscharow <jan.gontscharow@gmail.com>
2023-03-02 18:20:36 +0100

    added adjusted IndexAutomaton rewrite

---
+++ src/IndexAutomaton.jl
@@ -0,0 +1,238 @@
+mutable struct State{T, S}
+    transitions::Vector{State{T,S}} # vector of fixed size
+	fail::Bool
+    data::T
+    value::S
+
+	function State{T, S}(n::Integer; fail::Bool=false) where {T, S}
+        new{T,S}(Vector{State{T,S}}(undef, n), fail)
+    end
+    function State{T, S}(n::Integer, data; fail::Bool=false) where {T, S}
+        new{T,S}(Vector{State{T,S}}(undef, n), fail, data)
+    end
+end
+
+isterminal(s::State) = isdefined(s, :value)
+isfail(s::State) = s.fail
+
+	# σ[i]
+function Base.getindex(s::State, i::Integer)
+	isfail(s) && return s # return nothing
+	!isassigned(s.transitions, i) && return nothing
+	return s.transitions[i]
+end
+	# σ[i] = τ
+Base.setindex!(s::State, v::State, i::Integer) = s.transitions[i] = v
+
+hasedge(s::State, i::Integer) = !isnothing(s[i])
+	
+function value(s::State)
+    isterminal(s) && return s.value
+	throw("state is not terminal and its value is not assigned")
+end
+
+max_degree(s::State) = length(s.transitions)
+
+	# outdegree?
+degree(s::State) = count(i->hasedge(s, i), 1:max_degree(s))
+# transitions(s::State) = (s[i] for i in 1:max_degree(s) if hasedge(s, i))
+	
+function Base.show(io::IO, s::State)
+	if isterminal(s)
+		print(io, "terminal state: ", value(s))
+	else
+		print(io, "state (data=", s.data, ") with ", degree(s), " transitions")
+	end
+end
+
+
+
+
+abstract type Automaton end
+
+
+struct IndexAutomaton{T,S} <: Automaton
+	input_size::Int
+    initial::State{T,S}
+end
+
+# needed if the letter is negative
+function letter_to_index(l::Int, n::Int)
+	if l<0
+		return Int(-l+(n/2))
+	else
+		return l
+	end
+end
+letter_to_index(idxA::IndexAutomaton, l::Int) = letter_to_index(l, input_size(idxA))
+
+initial(idxA::IndexAutomaton) = idxA.initial
+input_size(idxA::IndexAutomaton) = idxA.input_size
+
+hasedge(::IndexAutomaton, σ::State, label::Integer) = hasedge(σ, label)
+trace(::IndexAutomaton, label::Integer, σ::State) = σ[label]
+
+
+"""
+
+"""
+function IndexAutomaton(R::RewritingSystem, n::Int)
+	# n is the degree so we need 2*n transitions
+	α = State{UInt32, Rule}(2*n, 0)
+    idxA = IndexAutomaton(2*n, α)
+	append!(idxA, rwrules(R))
+
+    return idxA
+end
+
+function Base.append!(idxA::IndexAutomaton, rules)
+	idxA, signatures = direct_edges!(idxA, rules)
+	idxA = skew_edges!(idxA, signatures) # complete!
+	return idxA
+end
+
+
+"""
+	hasedge(A::Automaton, σ, label)
+Check if `A` contains an edge starting at `σ` labeled by `label` 
+"""
+function hasedge(A::Automaton, σ, label) 
+	return !isnothing(σ[label])
+end
+
+"""
+	trace(A::Automaton, label, σ)
+Return `τ` if `(σ, label, τ)` is in `A`, otherwise return nothing.
+"""
+function trace(A::Automaton, label, σ) 
+	if hasedge(A, σ, label)
+		return σ[label]
+	end 
+end
+
+"""
+	trace(A::Automaton, w::AbstractVector{<:Integer} [, σ=initial(A)])
+Return a pair `(l, τ)`, where 
+ * `l` is the length of the longest prefix of `w` which defines a path starting at `σ` in `A` and
+ * `τ` is the last state (node) on the path.
+"""
+function trace(A::Automaton, w::AbstractVector, σ=initial(A))
+	for (i, l) in enumerate(w)
+		if hasedge(A, σ, l)
+			σ = trace(A, l, σ)
+		else
+			return i-1, σ
+		end
+	end
+	return length(w), σ
+end
+
+
+function direct_edges!(idxA::IndexAutomaton, rwrules)
+	@assert !isempty(rwrules)
+	#W = typeof(first(first(rwrules)))
+    α = initial(idxA)
+	S = typeof(α)
+	n = max_degree(α)
+	states_prefixes = [α=>word""] # will be kept sorted
+	for r in rwrules
+        lhs, _ = r
+        σ = α
+        for (prefix_length, l) in enumerate(lhs)
+			# shifting to positive range
+			#@info "$lhs, $l"
+			l = letter_to_index(l, n)
+			#@info "$lhs, $l"
+            if !hasedge(idxA, σ, l)
+				τ = S(n, prefix_length)
+				σ[l] = τ
+				st_prefix = τ=>MyWord(lhs[1:prefix_length])
+				# insert into sorted list
+				k = searchsortedfirst(
+					states_prefixes, 
+					st_prefix, 
+					by=n->first(n).data
+				)
+				#@info "$(eltype(states_prefixes)), $(typeof(st_prefix))"
+ 				insert!(states_prefixes, k, st_prefix)
+            end
+            σ = σ[l]
+        end
+        σ.value = r
+    end
+	return idxA, states_prefixes
+end
+
+function skew_edges!(idxA, states_prefixes)
+	# add missing loops at the root
+	α = initial(idxA)
+	if degree(α) ≠ max_degree(α)
+		for x in 1:max_degree(α)
+			if !hasedge(idxA, α, x)
+				# addedge!(idxA, (α, x, α))
+				α[x] = α
+			end
+		end
+	end
+
+	# this has to be done in breadth-first fashion so that
+	# trace(U, A) is defined
+	if !issorted(states_prefixes, by=n->first(n).data)
+		sort!(states_prefixes, by=n->first(n).data)
+	end
+	for (σ, prefix) in states_prefixes
+		degree(σ) == max_degree(σ) && continue
+		
+		#@info "$σ, $(prefix)"
+
+		τ = let U = prefix[2:end]
+			#@info "$l, $τ, $U"
+			U = map(x->letter_to_index(idxA,x), U)
+			#@info "$l, $τ, $U"
+			l, τ = trace(idxA, U)
+			@assert l == length(U) # the whole U defines a path in A
+			τ
+		end
+
+		for x in 1:max_degree(σ)
+			hasedge(idxA, σ, x) && continue
+			@assert hasedge(idxA, τ, x)
+			# addedge!(idxA, (σ, x, τ[x]))
+			σ[x] = τ[x]
+		end
+	end
+	return idxA
+end
+
+rewrite(w::MyWord, idxA::IndexAutomaton) = rewrite!(one(w), w, idxA)
+function rewrite!(
+	v::MyWord, 
+	w::MyWord, 
+	idxA::IndexAutomaton;
+	path=[initial(idxA)]
+)
+	resize!(v, 0)
+	while !isone(w)
+		x = popfirst!(w)
+		σ = last(path) # current state
+		τ = σ[x] # next state
+		@assert !isnothing(τ) "ia doesn't seem to be complete!; $σ"
+		
+		if isterminal(τ)
+			lhs, rhs = value(τ)
+			# lhs is a suffix of v·x, so we delete it from v
+			resize!(v, length(v) - length(lhs) + 1)
+			# now we need to rewind the path
+			resize!(path, length(path) - length(lhs) + 1)
+			# and prepend rhs to w
+			prepend!(w, rhs)
+			
+			# @assert trace(v, ia) == (length(v), last(path))
+		else
+			push!(v, x)
+			push!(path, τ)
+		end
+	end
+	return v	
+end
+
---
+++ test/IndexAutomaton.jl
@@ -0,0 +1,58 @@
+using Random
+
+@testset "State" begin
+	S = P.State{UInt32, P.Rule}
+	σ = S(2)
+	τ1 = S(2, 3)
+	τ2 = S(2, 4)
+	σ[1] = τ1
+	σ[2] = τ2
+
+	@test P.hasedge(σ, 1)
+	@test P.hasedge(σ, 2)
+	@test P.max_degree(τ1) == 2
+	@test P.degree(τ1) == 0 
+end
+
+@testset "IndexAutomaton" begin
+	S = P.State{UInt32, P.Rule}
+	σ = S(2)
+	σ1 = S(2)
+	σ2 = S(2)
+	σ[1] = σ1
+	σ[2] = σ2
+	idxA = P.IndexAutomaton(1, σ)
+
+	@test P.trace(idxA, 1, σ) == σ1
+	@test P.trace(idxA, 2, σ) == σ2
+	@test isnothing(P.trace(idxA, 1, σ1)) 
+
+	σ12 = S(1)
+	σ121 = S(0)
+	σ1[2] = σ12
+	σ12[1] = σ121
+	@test P.trace(idxA, [1,2,2], σ) == (2, σ12)
+	@test P.trace(idxA, [1,2,1], σ) == (3, σ121)
+end
+
+@testset "Index rewrite" begin
+	rws = let
+		a = P.word"a"
+        A = inv(a)
+		b = P.word"b"
+        B = inv(b)
+		ε = P.word""
+		rws = P.RewritingSystem(
+			[a*A=>ε, A*a=>ε, b*B=>ε, B*b=>ε, b*a=>a*b]
+		)
+		P.reduce(P.knuthbendix(rws))
+	end
+
+	idxA = P.IndexAutomaton(rws, 2)
+	n,l = (20,200)
+
+	for i in 1:n
+		w = P.MyWord(rand(1:2, l))
+		@test P.rewrite(w, rws) == P.rewrite(w, idxA)
+	end
+end
